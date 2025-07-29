c375ae37772619bc1f7a979afe47d5cfece0e492
ila-i <grasso.ilaria4@gmail.com>
2025-07-29 10:46:04 +0200

    modifiche su subscription

--- homepage/profile_creation/subscription.py
+++ homepage/profile_creation/subscription.py
@@ -1,4 +1,14 @@
-class TesseraSanitaria :
+from abc import ABC, abstractmethod
+
+class Persona (ABC) :
+    nome: str
+    cognome: str
+
+    def __init__(self):
+        self.nome = input("Inserire il proprio nome : ")
+        self.cognome = input("Inserire il proprio cognome : ")
+
+class TesseraSanitaria(Documento) :
     codice_fiscale: str
     sesso: str
     luogo_nascita: str
@@ -11,9 +21,7 @@ class TesseraSanitaria :
        parametro = input(messaggio)
        while len(parametro) != lunghezza :
             parametro = input(f" il parametro non è valido, riprovare : ")
-
        return parametro
-
 
 #TODO quando possibile inserire controllo data di nascita e scadenza tessera sanitaria
 
@@ -21,23 +29,27 @@ class TesseraSanitaria :
         print( " Di seguito si inseriscano i dati della tessera sanitaria : ")
         self.codice_fiscale = self.controllo(" CODICE FISCALE :", 16) # nel codice fiscale si contano 16 caratteri alfanumerici
         self.sesso = self.controllo(" SESSO : ", 1)
-        self.luogo_nascita = input(" LUOGO DI NASCITA : ")
-        self.provincia = self.controllo(" PROVINCIA : ", 2)
-        self.data_nascita = self.controllo(" DATA DI NASCITA (gg/mm/aaaa) : ", 10)
+        #self.luogo_nascita = input(" LUOGO DI NASCITA : ")
+        #self.provincia = self.controllo(" PROVINCIA : ", 2)
+        #self.data_nascita = self.controllo(" DATA DI NASCITA (gg/mm/aaaa) : ", 10)
         self.data_scadenza = self.controllo(" DATA DI SCADENZA (gg/mm/aaaa) : ", 10)
         self.numero_identificazione_tessera = self.controllo(" NUMERO IDENTIFICAZIONE TESSERA : ", 20)# sulla tessera sanitaria fisica sono 20 caratteri alfanumerici
 
-
-class Cliente :
-    t_s: TesseraSanitaria #t_s abbreviazione tessera sanitaria
-    nome:str
-    cognome:str
+class TesserinoProfessionale :
+    ordine_di_appartenenza: str
+    n_iscrizione_albo : str
 
     def __init__(self):
-        self.nome= input("Inserire il proprio nome : ")
-        self.cognome = input("Inserire il proprio cognome : ")
+        self.n_iscrizione_albo = input
+
+
+class Cliente(Persona):
+    t_s: TesseraSanitaria #t_s abbreviazione tessera sanitaria
+
+    def __init__(self):
+        super().__init__()
         self.t_s = TesseraSanitaria()
-    #def ricerca ()# da database per controllo su presenza o meno della persona
+    #def ricerca () # da database per controllo su presenza o meno della persona
 
 class ProfiloUtente :
     nome_utente:str
@@ -70,7 +82,7 @@ def registrarsi (lista_clienti : list[Cliente]) -> ProfiloUtente :
 
 
 
-
+#verifica di funzioanmento del codice
 print("inizializzazione lista ")
 lista = []
 controllo = "go"
